b1d8482ae4b6f353e6da4d8c166ce5c396231536
Muttaqin <muttaqin0708@gmail.com>
2023-11-24 10:32:41 +0700

    Fix sitemap blade.php

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,9 +1,9 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    @foreach ($blog as $blog)
+    @foreach ($blog as $blogItem)
         <url>
-            <loc>{{ url('/blog/' . $blog->slug) . '.html' }}</loc>
-            <lastmod>{{ $blog->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+            <loc>{{ url('/blog/' . $blogItem->slug) . '.html' }}</loc>
+            <lastmod>{{ $blogItem->updated_at->tz('UTC')->toAtomString() }}</lastmod>
         </url>
     @endforeach
 </urlset>
