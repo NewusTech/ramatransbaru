8075d720048d9b4ad4cca10675d123d8ca89964f
Muttaqin <muttaqin0708@gmail.com>
2023-11-24 13:24:06 +0700

    Update sitemap blade.php 2

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -4,16 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 use App\Models\Blog;
+use App\Models\Layanan;
+use App\Models\JenisLayanan;
+use App\Models\ParentArea;
 class SitemapController extends Controller
 {
     public function index()
     {
-        $blog = Blog::all(); 
-        $sitemap = view('sitemap', compact('blog'))->render();
+        $routes = collect(Route::getRoutes())->map(function ($route) {
+            return $route->uri();
+        });
 
-        return Response::make($sitemap, 200, [
-            'Content-Type' => 'application/xml',
-        ]);
+        $data = $routes->map(function ($route) {
+            return [
+                'loc' => url($route),
+                'lastmod' => now()->toAtomString(),
+            ];
+        });
+
+        $blog = Blog::all(); 
+        $layanan = Layanan::all();
+        $jenisLayanan = JenisLayanan::all();
+        $lokasi = ParentArea::all();
+        
+        $xml = View::make('sitemap', compact('data', 'blog', 'layanan', 'jenisLayanan', 'lokasi'))->render();
+
+        return Response::make($xml, 200, ['Content-Type' => 'application/xml']);
     }
 }
