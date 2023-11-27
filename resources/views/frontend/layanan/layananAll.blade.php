c782fd71c08f5394a0626917144ac03a9341f9bf
Muttaqin <muttaqin0708@gmail.com>
2023-11-27 15:16:14 +0700

    fix sitemap controller, hapus beberapa link penyebab title issue

--- resources/views/frontend/layanan/layananAll.blade.php
+++ resources/views/frontend/layanan/layananAll.blade.php
@@ -322,7 +322,7 @@
                     },
                     success: function(data) {
                         $('#layananAll-list').html(data);
-                        document.title = "Rama Trans Travel - Rute | Page " + pageNumber;
+                        document.title = "Rama Trans Travel - Rute | Page " + page;
                     }
                 });
             }
