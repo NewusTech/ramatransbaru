26e18dd00f56ff2e5d52cbbf31749bd7bd8fd984
muttaqin0708 <muttaqin0708@gmail.com>
2024-02-26 12:46:52 +0700

    Update text editor

--- resources/views/livewire/backend/blog/form-bloASDASDg.blade.php
+++ resources/views/livewire/backend/blog/form-bloASDASDg.blade.php
@@ -104,8 +104,8 @@
                                 </div>
                                 <div class="form-group" wire:ignore>
                                     <label for="">Konten</label>
-                                    <textarea class="form-control body @error('blogs.content') is-invalid @enderror" style="height: 500px" rows="10"
-                                        data-model="blogs.content" wire:model="blogs.content"> {{ $blogs ? $blogs->content : '' }}</textarea>
+                                    <textarea class="form-control tinyMCE @error('blogs.content') is-invalid @enderror" data-model="blogs.content"
+                                        wire:model="blogs.content">{{ $blogs ? $blogs->content : '' }}</textarea>
                                     @error('blogs.content')
                                         <span class='invalid-feedback'>
                                             <strong>{{ $message }} </strong>
@@ -128,13 +128,13 @@
                                         <div class="mt-2">
                                             <label for="preview">Preview:</label>
                                             <img src="{{ $gambar->temporaryUrl() }}" alt="Preview"
-                                                class="img-thumbnail" width="300px" height="auto">
+                                                class="img-thumbnail">
                                         </div>
                                     @elseif ($blogs->image)
                                         <div class="mt-2">
                                             <label for="currentImage">Current Image:</label>
                                             <img src="{{ Storage::disk('s3')->url($blogs->image) }}"
-                                                alt="Current Image" class="img-thumbnail" width="300px" height="auto">
+                                                alt="Current Image" class="img-thumbnail">
                                         </div>
                                     @endif
                                 </div>
@@ -148,169 +148,32 @@
                 </div>
             </div>
         </section>
+
+
     </div>
+
+
 
 </div>
 
 
 @push('scripts')
-    <script src="https://cdn.ckeditor.com/ckeditor5/35.3.2/super-build/ckeditor.js"></script>
+    {{-- <script src="https://cdn.tiny.cloud/1/02ycb6jfsha69obgdavcwnfnm8bna9ez9cthzwha1gagrflu/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script> --}}
     <script>
-        CKEDITOR.ClassicEditor.create(document.querySelector('textarea.body'), {
-            toolbar: {
-                items: [
-                    'exportPDF', 'exportWord', '|',
-                    'findAndReplace', 'selectAll', '|',
-                    'heading', '|',
-                    'bold', 'italic', 'strikethrough', 'underline', 'code', 'subscript', 'superscript',
-                    'removeFormat', '|',
-                    'bulletedList', 'numberedList', 'todoList', '|',
-                    'outdent', 'indent', '|',
-                    'undo', 'redo',
-                    '-',
-                    'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'highlight', '|',
-                    'alignment', '|',
-                    'link', 'insertImage', 'blockQuote', 'insertTable', 'mediaEmbed', 'codeBlock', 'htmlEmbed',
-                    '|',
-                    'specialCharacters', 'horizontalLine', 'pageBreak', '|',
-                    'textPartLanguage', '|',
-                    'sourceEditing'
-                ],
-                shouldNotGroupWhenFull: true
-            },
-
-            list: {
-                properties: {
-                    styles: true,
-                    startIndex: true,
-                    reversed: true
-                }
-            },
-            heading: {
-                options: [{
-                        model: 'paragraph',
-                        title: 'Paragraph',
-                        class: 'ck-heading_paragraph'
-                    },
-                    {
-                        model: 'heading1',
-                        view: 'h1',
-                        title: 'Heading 1',
-                        class: 'ck-heading_heading1'
-                    },
-                    {
-                        model: 'heading2',
-                        view: 'h2',
-                        title: 'Heading 2',
-                        class: 'ck-heading_heading2'
-                    },
-                    {
-                        model: 'heading3',
-                        view: 'h3',
-                        title: 'Heading 3',
-                        class: 'ck-heading_heading3'
-                    },
-                    {
-                        model: 'heading4',
-                        view: 'h4',
-                        title: 'Heading 4',
-                        class: 'ck-heading_heading4'
-                    },
-                    {
-                        model: 'heading5',
-                        view: 'h5',
-                        title: 'Heading 5',
-                        class: 'ck-heading_heading5'
-                    },
-                    {
-                        model: 'heading6',
-                        view: 'h6',
-                        title: 'Heading 6',
-                        class: 'ck-heading_heading6'
-                    }
-                ]
-            },
-            fontFamily: {
-                options: [
-                    'default',
-                    'Arial, Helvetica, sans-serif',
-                    'Courier New, Courier, monospace',
-                    'Georgia, serif',
-                    'Lucida Sans Unicode, Lucida Grande, sans-serif',
-                    'Tahoma, Geneva, sans-serif',
-                    'Times New Roman, Times, serif',
-                    'Trebuchet MS, Helvetica, sans-serif',
-                    'Verdana, Geneva, sans-serif'
-                ],
-                supportAllValues: true
-            },
-            fontSize: {
-                options: [10, 12, 14, 'default', 18, 20, 22],
-                supportAllValues: true
-            },
-            htmlSupport: {
-                allow: [{
-                    name: /.*/,
-                    attributes: true,
-                    classes: true,
-                    styles: true,
-                }]
-            },
-            // Be careful with enabling previews
-            // https://ckeditor.com/docs/ckeditor5/latest/features/html-embed.html#content-previews
-            htmlEmbed: {
-                showPreviews: true
-            },
-            // https://ckeditor.com/docs/ckeditor5/latest/features/link.html#custom-link-attributes-decorators
-            link: {
-                decorators: {
-                    addTargetToExternalLinks: true,
-                    defaultProtocol: 'https://',
-                    toggleDownloadable: {
-                        mode: 'manual',
-                        label: 'Downloadable',
-                        attributes: {
-                            download: 'image'
-                        }
-                    }
-                }
-            },
-            // https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html#configuration
-            mention: {
-                feeds: [{
-                    marker: '@',
-                    feed: [
-                        '@apple', '@bears', '@brownie', '@cake', '@cake', '@candy', '@canes',
-                        '@chocolate', '@cookie', '@cotton', '@cream',
-                        '@cupcake', '@danish', '@donut', '@dragée', '@fruitcake', '@gingerbread',
-                        '@gummi', '@ice', '@jelly-o',
-                        '@liquorice', '@macaroon', '@marzipan', '@oat', '@pie', '@plum', '@pudding',
-                        '@sesame', '@snaps', '@soufflé',
-                        '@sugar', '@sweet', '@topping', '@wafer'
-                    ],
-                    minimumCharacters: 1
-                }]
-            },
-            removePlugins: [
-                'CKBox',
-                'CKFinder',
-                'EasyImage',
-                'RealTimeCollaborativeComments',
-                'RealTimeCollaborativeTrackChanges',
-                'RealTimeCollaborativeRevisionHistory',
-                'PresenceList',
-                'Comments',
-                'TrackChanges',
-                'TrackChangesData',
-                'RevisionHistory',
-                'Pagination',
-                'WProofreader',
-                'MathType'
-            ]
-        }).then(editor => {
-            editor.model.document.on('change:data', () => {
-                @this.set('blogs.content', editor.getData());
-            });
-        })
+        tinymce.init({
+            selector: 'textarea.tinyMCE', // Replace this CSS selector to match the placeholder element for TinyMCE
+            plugins: 'advlist autolink lists link image charmap preview searchreplace visualblocks code codesample fullscreen insertdatetime media table wordcount',
+            toolbar: 'fullscreen code preview | undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | numlist bullist | forecolor backcolor removeformat | image media link | wordcount',
+            tinycomments_mode: 'embedded',
+            tinycomments_author: 'Rama Trans',
+            setup: function(editor) {
+                editor.on('init change', function() {
+                    editor.save();
+                });
+                editor.on('change', function(e) {
+                    @this.set('blogs.content', editor.getContent());
+                });
+            }
+        });
     </script>
 @endpush
--- resources/views/livewire/backend/jenis-layanan/form-jenis-layanan.blade.php
+++ resources/views/livewire/backend/jenis-layanan/form-jenis-layanan.blade.php
@@ -65,27 +65,25 @@
                                                 <div class="mt-2">
                                                     <label for="preview">Preview:</label>
                                                     <img src="{{ $gambar->temporaryUrl() }}" alt="Preview"
-                                                        class="img-thumbnail">
+                                                        class="img-thumbnail" width="300px" height="auto">
                                                 </div>
                                             @elseif ($jenisLayanan->media)
                                                 Current: <br>
                                                 <img src="{{ Storage::disk('s3')->url($jenisLayanan->media) }}"
-                                                    alt="Current Image" class="img-thumbnail">
+                                                    alt="Current Image" class="img-thumbnail" width="300px" height="auto">
                                             @endif
                                         </div>
 
                                         <div class="form-group" wire:ignore>
                                             <label for="">Isi Konten</label>
-                                            <textarea name="" id="" class="form-control tinyMCE @error('jenisLayanan.content') is-invalid @enderror"
-                                                data-model="jenisLayanan.content" wire:model="jenisLayanan.content">{{ $jenisLayanan ? $jenisLayanan->content : '' }}</textarea>
+                                            <textarea class="form-control body @error('jenisLayanan.content') is-invalid @enderror" style="height: 500px" rows="10"
+                                                data-model="jenisLayanan.content" wire:model="jenisLayanan.content"> {{ $jenisLayanan ? $jenisLayanan->content : '' }}</textarea>
                                             @error('jenisLayanan.content')
                                                 <span class='invalid-feedback'>
                                                     <strong>{{ $message }} </strong>
                                                 </span>
                                             @enderror
                                         </div>
-
-
                                     </div>
                                 </div>
                             </div>
@@ -102,60 +100,163 @@
 
 
 @push('scripts')
+    <script src="https://cdn.ckeditor.com/ckeditor5/35.3.2/super-build/ckeditor.js"></script>
     <script>
-        window.initSummernote = () => {
-            $(".wysiwyg").summernote({
-                toolbar: [
-                    ['style', ['style']],
-                    ['font', ['bold', 'underline', 'clear']],
-                    ['fontname', ['fontname']],
-                    ['color', ['color']],
-                    ['para', ['ul', 'ol', 'paragraph']],
-                    ['table', ['table']],
-                    ['insert', ['link', 'picture', 'video']],
-                    ['view', ['fullscreen', 'help']],
+        CKEDITOR.ClassicEditor.create(document.querySelector('textarea.body'), {
+            toolbar: {
+                items: [
+                    'exportPDF', 'exportWord', '|',
+                    'findAndReplace', 'selectAll', '|',
+                    'heading', '|',
+                    'bold', 'italic', 'strikethrough', 'underline', 'code', 'subscript', 'superscript',
+                    'removeFormat', '|',
+                    'bulletedList', 'numberedList', 'todoList', '|',
+                    'outdent', 'indent', '|',
+                    'undo', 'redo',
+                    '-',
+                    'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'highlight', '|',
+                    'alignment', '|',
+                    'link', 'insertImage', 'blockQuote', 'insertTable', 'mediaEmbed', 'codeBlock', 'htmlEmbed',
+                    '|',
+                    'specialCharacters', 'horizontalLine', 'pageBreak', '|',
+                    'textPartLanguage', '|',
+                    'sourceEditing'
                 ],
-                height: 200,
-                dialogsInBody: true
+                shouldNotGroupWhenFull: true
+            },
+
+            list: {
+                properties: {
+                    styles: true,
+                    startIndex: true,
+                    reversed: true
+                }
+            },
+            heading: {
+                options: [{
+                        model: 'paragraph',
+                        title: 'Paragraph',
+                        class: 'ck-heading_paragraph'
+                    },
+                    {
+                        model: 'heading1',
+                        view: 'h1',
+                        title: 'Heading 1',
+                        class: 'ck-heading_heading1'
+                    },
+                    {
+                        model: 'heading2',
+                        view: 'h2',
+                        title: 'Heading 2',
+                        class: 'ck-heading_heading2'
+                    },
+                    {
+                        model: 'heading3',
+                        view: 'h3',
+                        title: 'Heading 3',
+                        class: 'ck-heading_heading3'
+                    },
+                    {
+                        model: 'heading4',
+                        view: 'h4',
+                        title: 'Heading 4',
+                        class: 'ck-heading_heading4'
+                    },
+                    {
+                        model: 'heading5',
+                        view: 'h5',
+                        title: 'Heading 5',
+                        class: 'ck-heading_heading5'
+                    },
+                    {
+                        model: 'heading6',
+                        view: 'h6',
+                        title: 'Heading 6',
+                        class: 'ck-heading_heading6'
+                    }
+                ]
+            },
+            fontFamily: {
+                options: [
+                    'default',
+                    'Arial, Helvetica, sans-serif',
+                    'Courier New, Courier, monospace',
+                    'Georgia, serif',
+                    'Lucida Sans Unicode, Lucida Grande, sans-serif',
+                    'Tahoma, Geneva, sans-serif',
+                    'Times New Roman, Times, serif',
+                    'Trebuchet MS, Helvetica, sans-serif',
+                    'Verdana, Geneva, sans-serif'
+                ],
+                supportAllValues: true
+            },
+            fontSize: {
+                options: [10, 12, 14, 'default', 18, 20, 22],
+                supportAllValues: true
+            },
+            htmlSupport: {
+                allow: [{
+                    name: /.*/,
+                    attributes: true,
+                    classes: true,
+                    styles: true,
+                }]
+            },
+            // Be careful with enabling previews
+            // https://ckeditor.com/docs/ckeditor5/latest/features/html-embed.html#content-previews
+            htmlEmbed: {
+                showPreviews: true
+            },
+            // https://ckeditor.com/docs/ckeditor5/latest/features/link.html#custom-link-attributes-decorators
+            link: {
+                decorators: {
+                    addTargetToExternalLinks: true,
+                    defaultProtocol: 'https://',
+                    toggleDownloadable: {
+                        mode: 'manual',
+                        label: 'Downloadable',
+                        attributes: {
+                            download: 'image'
+                        }
+                    }
+                }
+            },
+            // https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html#configuration
+            mention: {
+                feeds: [{
+                    marker: '@',
+                    feed: [
+                        '@apple', '@bears', '@brownie', '@cake', '@cake', '@candy', '@canes',
+                        '@chocolate', '@cookie', '@cotton', '@cream',
+                        '@cupcake', '@danish', '@donut', '@dragée', '@fruitcake', '@gingerbread',
+                        '@gummi', '@ice', '@jelly-o',
+                        '@liquorice', '@macaroon', '@marzipan', '@oat', '@pie', '@plum', '@pudding',
+                        '@sesame', '@snaps', '@soufflé',
+                        '@sugar', '@sweet', '@topping', '@wafer'
+                    ],
+                    minimumCharacters: 1
+                }]
+            },
+            removePlugins: [
+                'CKBox',
+                'CKFinder',
+                'EasyImage',
+                'RealTimeCollaborativeComments',
+                'RealTimeCollaborativeTrackChanges',
+                'RealTimeCollaborativeRevisionHistory',
+                'PresenceList',
+                'Comments',
+                'TrackChanges',
+                'TrackChangesData',
+                'RevisionHistory',
+                'Pagination',
+                'WProofreader',
+                'MathType'
+            ]
+        }).then(editor => {
+            editor.model.document.on('change:data', () => {
+                @this.set('jenisLayanan.content', editor.getData());
             });
-        }
-
-        initSummernote();
-        window.livewire.on('summernote', () => {
-            initSummernote();
-        });
-
-        window.addEventListener('summernote', function() {
-            initSummernote();
         })
-
-        $(".wysiwyg").on('summernote.blur', function() {
-            val = $(this).val();
-            @this.set($(this).data('model'), val);
-            console.log(val);
-        })
-
-        // Add the following code if you want the name of the file appear on select
-        $(".custom-file-input").on("change", function() {
-            var fileName = $(this).val().split("\\").pop();
-            $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-        });
-    </script>
-
-    <script src="https://cdn.tiny.cloud/1/02ycb6jfsha69obgdavcwnfnm8bna9ez9cthzwha1gagrflu/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-    <script>
-        tinymce.init({
-            selector: 'textarea.tinyMCE', // Replace this CSS selector to match the placeholder element for TinyMCE
-            plugins: 'advlist autolink lists link image charmap preview searchreplace visualblocks code codesample fullscreen insertdatetime media table wordcount',
-            toolbar: 'fullscreen code preview | undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | numlist bullist | forecolor backcolor removeformat | image media link | wordcount',
-            setup: function(editor) {
-                editor.on('init change', function() {
-                    editor.save();
-                });
-                editor.on('change', function(e) {
-                    @this.set('jenisLayanan.content', editor.getContent());
-                });
-            }
-        });
     </script>
 @endpush
