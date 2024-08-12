eed1394cd00290d4412783abdde755e1cac3b3f4
Gilang Rizky Ramadhan <gilang.119140081@student.itera.ac.id>
2024-08-12 15:46:11 +0700

    update controller

--- app/Http/Livewire/Dashboard/DashboardSuperadmin.php
+++ app/Http/Livewire/Dashboard/DashboardSuperadmin.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Dashboard;
 use App\Models\Blog;
 use App\Models\JenisLayanan;
 use App\Models\Layanan;
+use App\Models\ListOrder;
 use Livewire\Component;
 
 class DashboardSuperadmin extends Component
@@ -17,6 +18,7 @@ class DashboardSuperadmin extends Component
         $this->layanan = Layanan::get();
         $this->jenisLayanan = JenisLayanan::get();
         $this->blogs = Blog::get();
+        $this->ListOrders = ListOrder::get();
     }
 
     public function render()
