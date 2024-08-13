ac4d1a60f7ece14613b4b4547bf2814424d7684a
Gilang Rizky Ramadhan <gilang.119140081@student.itera.ac.id>
2024-08-13 17:06:57 +0700

    update image layanan

--- resources/views/frontend/beranda/index2.blade.php
+++ resources/views/frontend/beranda/index2.blade.php
@@ -340,7 +340,7 @@
                 
                 @foreach ($jenisLayanan as $item)
                 <div class="card pt-1 pb-1"> <!-- Menambahkan padding atas dan bawah pada card -->
-                    <img src="img/car.jpg" class="card-img-top" alt="..." style="height: 180px; object-fit: cover;"> <!-- Mengatur tinggi gambar -->
+                    <img src="{{ Storage::disk('s3')->url($layanans->image) }}" class="card-img-top" alt="..." style="height: 180px; object-fit: cover;"> <!-- Mengatur tinggi gambar -->
                     <div class="card-body">
                         <h5 class="card-title">{{ $item->title }}</h5>
                         <p class="card-text">{!! Str::words($item->content, 40, '...') !!}</p>
