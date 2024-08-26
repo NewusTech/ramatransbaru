39c412ad2b12aaf894cf51691b40e682d1acbc71
Gilang Rizky Ramadhan <gilang.119140081@student.itera.ac.id>
2024-08-26 15:48:47 +0700

    update card2

--- resources/views/frontend/beranda/index2.blade.php
+++ resources/views/frontend/beranda/index2.blade.php
@@ -401,7 +401,6 @@
             <span class="sr-only">Next</span>
         </a>
     </div>
-    <br>
     <a href="#" class="btn btn-primary w-100" data-toggle="modal" data-item="{{ $layanans }}" data-target="#modalBookingIndex">
                             Pesan
                         </a>
